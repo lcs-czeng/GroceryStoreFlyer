6080208126f9d6034bfafc388090fd535d2c7036
Courage Zeng <czeng@lcs.on.ca>
2025-01-09 14:22:47 -0500

    Initial Commit

---
+++ GroceryStoreFlyer/GroceryStoreFlyerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryStoreFlyerApp.swift
+//  GroceryStoreFlyer
+//
+//  Created by 曾梓恒 on 2025-01-09.
+//
+
+import SwiftUI
+
+@main
+struct GroceryStoreFlyerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
